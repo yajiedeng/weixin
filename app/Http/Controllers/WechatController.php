9bee5b8e7295096b59fb8951089c9d30c75b8aaa
dengyajie <dengyajie@incar100.com>
2018-05-22 13:40:58 +0800

    update overtrue/laravel-wechat

--- app/Http/Controllers/WechatController.php
+++ app/Http/Controllers/WechatController.php
@@ -11,16 +11,23 @@ class WechatController extends Controller
     {
         Log::info('request arrived.'); # 注意：Log 为 Laravel 组件，所以它记的日志去 Laravel 日志看，而不是 EasyWeChat 日志
 
-        $wechat = app('wechat.official_account');
-//        dump($wechat);
-        $wechat->server->setMessageHandler(function($message){
+//        $wechat = app('wechat.official_account');
+////        dump($wechat);
+//        $wechat->server->setMessageHandler(function($message){
+//            return "欢迎关注 overtrue！";
+//        });
+//
+//        //3d779bed0bc68ec450749709e9c4324d
+//
+//        Log::info('return response.');
+//
+//        return $wechat->server->serve();
+
+        $app = app('wechat.official_account');
+        $app->server->push(function($message){
             return "欢迎关注 overtrue！";
         });
 
-        //3d779bed0bc68ec450749709e9c4324d
-
-        Log::info('return response.');
-
-        return $wechat->server->serve();
+        return $app->server->serve();
     }
 }
