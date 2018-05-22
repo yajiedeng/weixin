b76cd59cbd9bbe2d56c6b92154432515d32675c7
dengyajie <dengyajie@incar100.com>
2018-05-22 14:56:05 +0800

    update overtrue/laravel-wechat

--- app/Http/Controllers/WechatController.php
+++ app/Http/Controllers/WechatController.php
@@ -13,7 +13,35 @@ class WechatController extends Controller
 
         $app = app('wechat.official_account');
         $app->server->push(function($message){
-            return "哈哈哈哈 您刚才说 ".$message['Content'];
+            switch ($message['MsgType']) {
+                case 'event':
+                    return '收到事件消息';
+                    break;
+                case 'text':
+                    return '收到文字消息';
+                    break;
+                case 'image':
+                    return '收到图片消息';
+                    break;
+                case 'voice':
+                    return '收到语音消息';
+                    break;
+                case 'video':
+                    return '收到视频消息';
+                    break;
+                case 'location':
+                    return '收到坐标消息';
+                    break;
+                case 'link':
+                    return '收到链接消息';
+                    break;
+                case 'file':
+                    return '收到文件消息';
+                // ... 其它消息
+                default:
+                    return '收到其它消息';
+                    break;
+            }
         });
 
         return $app->server->serve();
