0c417f7246a3ca36407a8d1a801fd32b8a93133c
dengyajie <dengyajie@incar100.com>
2018-05-22 11:34:18 +0800

    add WechatController

---
+++ app/Http/Controllers/WechatController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Log;
+
+class WechatController extends Controller
+{
+    public function serve()
+    {
+        
+    }
+}
