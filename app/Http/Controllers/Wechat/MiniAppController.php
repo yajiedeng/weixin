0bcade89e42056aba9a6c6a0babfc35180fa420a
dengyajie <dengyajie@incar100.com>
2018-05-23 20:04:15 +0800

    miniapp

--- app/Http/Controllers/Wechat/MiniAppController.php
+++ app/Http/Controllers/Wechat/MiniAppController.php
@@ -5,18 +5,16 @@ namespace App\Http\Controllers\Wechat;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use EasyWeChat\Kernel\Messages\Text;
-use Log;
+
 class MiniAppController extends Controller
 {
     //小程序 客服接收消息&事件地址
     public function serve()
     {
-//        Log::info();
         $app = app('wechat.mini_program');
         $message = $app->server->getMessage();
         $openId = $message['FromUserName'];
         $text = new Text('hello');
         return $app->customer_service->message($text)->to($openId)->send();
-        return $app->server->serve();
     }
 }
