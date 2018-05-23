901653994725b29475e99c7f9afa556b6322e969
dengyajie <dengyajie@incar100.com>
2018-05-23 20:28:46 +0800

    miniapp

--- app/Http/Controllers/Wechat/MiniAppController.php
+++ app/Http/Controllers/Wechat/MiniAppController.php
@@ -15,7 +15,7 @@ class MiniAppController extends Controller
         $message = $app->server->getMessage();
         $openId = $message['FromUserName'];
         $text = new Text('hello');
-        echo $app->customer_service->message($text)->to($openId)->send();
+        return $app->customer_service->message($text)->to($openId)->send();
         return $app->server->serve();
     }
 }
