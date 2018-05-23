548aeca1ac975e178f8c666da9799630e23fbaab
dengyajie <dengyajie@incar100.com>
2018-05-23 17:24:47 +0800

    zhima view

--- app/Http/Controllers/Wechat/WechatController.php
+++ app/Http/Controllers/Wechat/WechatController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Wechat;
 
 use Illuminate\Http\Request;
 use Log;
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
+//前台路由组
+Route::group(['namespace' => 'Dadao'], function(){
+    Route::get('/zhima',"ZhimaController@zhima");
+});
