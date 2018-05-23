fca379ba4ceed6cb8ea91b3c757eedec90d34820
dengyajie <dengyajie@incar100.com>
2018-05-23 20:47:20 +0800

    miniapp

--- app/Http/Controllers/Wechat/MiniAppController.php
+++ app/Http/Controllers/Wechat/MiniAppController.php
@@ -13,10 +13,27 @@ class MiniAppController extends Controller
     {
         $app = app('wechat.mini_program');
 
+        $app = app('wechat.official_account');
+
         $message = $app->server->getMessage();
-        $openId = $message['FromUserName'];
-        $text = new Text('hello');
-        $app->customer_service->message($text)->to($openId)->send();
+
+        if(isset($message)){
+            $openId = $message['FromUserName'];
+            $text = new Text('hello');
+            $app->customer_service->message($text)->to($openId)->send();
+        }else{
+            $app->server->push(function ($message) {
+                // $message['FromUserName'] // 用户的 openid
+                // $message['MsgType'] // 消息类型：event, text....
+                return "hello";
+            });
+        }
+
         return $app->server->serve();
+
+
+
+
+
     }
 }
