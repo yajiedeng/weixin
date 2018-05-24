79146ff684b4eac024cec489dcb4ed333f2737d0
dengyajie <dengyajie@incar100.com>
2018-05-24 10:05:46 +0800

    update message

--- app/Http/Controllers/Wechat/MiniAppController.php
+++ app/Http/Controllers/Wechat/MiniAppController.php
@@ -9,33 +9,21 @@ use EasyWeChat\Kernel\Messages\Text;
 class MiniAppController extends Controller
 {
     //小程序 客服接收消息&事件地址
-    public function serve()
+    public function serve(Request $request)
     {
+        //初始化
         $app = app('wechat.mini_program');
-
-        $message = $app->server->getMessage();
-        $openId = $message['FromUserName'];
-        $text = new Text('hello');
-        $app->customer_service->message($text)->to($openId)->send();
-
-        $app->server->push(function ($message) {
-            // $message['FromUserName'] // 用户的 openid
-            // $message['MsgType'] // 消息类型：event, text....
-            return "hello";
-        });
-
-        return $app->server->serve();
-
-        if(isset($message)){
-            $openId = $message['FromUserName'];
-            $text = new Text('hello');
-            $app->customer_service->message($text)->to($openId)->send();
-        }else{
+        if($request->isMethod('get')){
             $app->server->push(function ($message) {
                 // $message['FromUserName'] // 用户的 openid
                 // $message['MsgType'] // 消息类型：event, text....
-                return "hello";
+                return "你好，大道用车为你服务，绑定芝麻信用请点击链接：https://wechat-oa.mydadao.com/zhima";
             });
+        }elseif($request->isMethod('post')){
+            $message = $app->server->getMessage();
+            $openId = $message['FromUserName'];
+            $text = new Text('你好，大道用车为你服务，绑定芝麻信用请点击链接：https://wechat-oa.mydadao.com/zhima');
+            $app->customer_service->message($text)->to($openId)->send();
         }
         return $app->server->serve();
     }
