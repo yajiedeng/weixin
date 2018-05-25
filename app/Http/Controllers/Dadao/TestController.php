35a4c901c3d483a657480b600d2e1abe2b35b3d9
dengyajie <dengyajie@incar100.com>
2018-05-25 11:43:34 +0800

    update message

--- app/Http/Controllers/Dadao/TestController.php
+++ app/Http/Controllers/Dadao/TestController.php
@@ -12,7 +12,37 @@ class TestController extends Controller
 {
     public function test(Request $request){
 
+        $arr = [
+            'title' => '收费标准',
+            'description' => '计价规则',
+            'url' => 'http://mp.weixin.qq.com/s?__biz=MzUxMzQ2OTkzMw==&mid=100000006&idx=1&sn=1f42ba0c2e686475ef607ce034d75d57&chksm=7955fd844e2274926b5a541ebf4aa50912d16a22ae34cbdc8d975ae964a0ed37fb9dc20debd1&scene=18#wechat_redirect',
+            'picurl' => 'https://mmbiz.qpic.cn/mmbiz_jpg/Fj8OO58Avd3p1NttnXIhoRuZHYFDicpS7O7ntgsTaVfYGZpOdolSWGvRth3UTWmbM54d16s3TLRYEM9VlmXnmtg/640?wx_fmt=jpeg&tp=webp&wxfrom=5&wx_lazy=1',
+        ];
+
+        $json = json_encode($arr);
+
+        $re = DB::table('wx_message')->insert(['keyword'=>1,'content'=>$json,'type'=>2]);
+        die;
+
+
+        $std = new \stdClass();
+        $std->name = 'jack';
+        $std->age = 18;
+        $std->sex = '男';
+
+        dump($std);
+        die;
+
         $data = DB::table('wx_message')->where('keyword',16)->first();
+        dump($data);
+        die;
+        if($data){
+            echo $data->title;
+        }else{
+            echo 'not objec';
+        }
+
+        die;
         if($data){
             dump($data);
         }else{
