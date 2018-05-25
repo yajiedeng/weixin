de09d9cbc716bb8088ab5eb62944a4db3ca3dc9d
dengyajie <dengyajie@incar100.com>
2018-05-25 14:26:22 +0800

    update menu controller

--- app/Http/Controllers/Wechat/MenuController.php
+++ app/Http/Controllers/Wechat/MenuController.php
@@ -22,4 +22,55 @@ class MenuController extends Controller
         $list = $this->app->menu->list();
         dump($list);
     }
+
+    /*
+     * 创建自定义菜单
+     * */
+    public function addMenu()
+    {
+        $reg_url = config('wechat_parameter.reg_url');
+        $buttons = [
+            [
+                "type" => "view",
+                "name" => "APP下载",
+                "url"  => $reg_url
+            ],
+            [
+                "name"       => "马上用车",
+                "sub_button" => [
+                    [
+                        "type" => "view",
+                        "name" => "领取礼包",
+                        "url"  => $reg_url
+                    ],
+                    [
+                        "type" => "view",
+                        "name" => "免押金及费用",
+                        "url"  => "http://mp.weixin.qq.com/s?__biz=MzUxMzQ2OTkzMw==&mid=100000136&idx=1&sn=c7a68f5383ba9eb3cc3fceb501d8df51&chksm=7955fd0a4e22741c146c89ec094ccf53254a45dfee6e043eee55f54bd3194456b80e59c0218d#rd"
+                    ],
+                    [
+                        "type" => "view",
+                        "name" => "Q&A",
+                        "url" => "http://mp.weixin.qq.com/s?__biz=MzUxMzQ2OTkzMw==&mid=100000010&idx=1&sn=554a66a3b743fbd133ca8e4ca7d65f50&chksm=7955fd884e22749e9b373e12b023a61b78a18747771aafa44d5072b61e8f566bd614bd48fd57&scene=18#wechat_redirect"
+                    ],
+                ],
+            ],
+            [
+                "name"       => "为您服务",
+                "sub_button" => [
+                    [
+                        "type" => "click",
+                        "name" => "客服热线",
+                        "key"  => "kefurexian"
+                    ],
+                    [
+                        "type" => "view",
+                        "name" => "违章处理",
+                        "url"  => "http://invite.mydadao.com/Illegal.html"
+                    ],
+                ],
+            ],
+        ];
+        $this->app->menu->create($buttons);
+    }
 }
