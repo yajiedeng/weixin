1e816e94eb9952606b01e4a9ea606e97dbf4a7d7
dengyajie <dengyajie@incar100.com>
2018-05-25 18:11:21 +0800

    update composer

--- routes/web.php
+++ routes/web.php
@@ -18,5 +18,5 @@ Route::get('/', function () {
 Route::group(['namespace' => 'Dadao'], function(){
     Route::get('/zhima',"ZhimaController@zhima");
     Route::get('/channel',"ChannelController@create");
-    Route::post('docreate',"ChannelController@qr");
+    Route::post('docreate',"ChannelController@doCreate");
 });
