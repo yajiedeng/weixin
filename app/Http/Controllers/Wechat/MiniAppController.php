22401c1ff021523e158bd48c67b9a43d110487b5
dengyajie <dengyajie@incar100.com>
2018-05-28 18:34:34 +0800

    channel

--- app/Http/Controllers/Wechat/MiniAppController.php
+++ app/Http/Controllers/Wechat/MiniAppController.php
@@ -48,7 +48,7 @@ class MiniAppController extends Controller
     public function getOpenid(Request $request){
         $method = $this->method;
         if($method != "POST"){
-            responce('502','The server rejected your request');
+            return responce('502','The server rejected your request');
             die;
         }
 
@@ -60,36 +60,35 @@ class MiniAppController extends Controller
         $result = $this->app->auth->session($code);
         Log::info('user '.$user_id.' wechat info '.json_encode($result));
         if($result['errcode'] == 40163){
-            responce($result['errcode'],$result['errmsg']);
-            die;
+            return responce($result['errcode'],$result['errmsg']);
         }
         //查询是否有该用户信息
         if($code == -1){//获取openid
             if($user){//用户信息存在返回openid
-                responce(200,'success',$user->openid);
+                return responce(200,'success',$user->openid);
                 die;
             }else{
-                responce(400,'not openid','');
+                return responce(400,'not openid','');
                 die;
             }
         }else{//存储openid
             //判断该用户openid是否存在
             if($user && $user->openid != 0){//openid 已存在
-                responce(200,'success',$user->openid);
+                return responce(200,'success',$user->openid);
             }elseif ($user && $user->openid == 0){//上次存储失败 重新存储
                 $user->user_id = $user_id;
                 $user->openid = $result['openid'];
                 $user->session_key = $result['session_key'];
 //                $user->unionid = $result['unionid'];
                 if($user->save()){
-                    responce(200,'success',$result['openid']);
+                    return responce(200,'success',$result['openid']);
                     die;
                 }
             }elseif(!$user){
 //                $user = User::create(['user_id'=>$user_id,'openid'=>$result['openid'],'session_key'=>$result['session_key'],'unionid'=>$result['unionid']]);
                 $user = User::create(['user_id'=>$user_id,'openid'=>$result['openid'],'session_key'=>$result['session_key']]);
                 if($user){
-                    responce(200,'success',$result['openid']);
+                    return responce(200,'success',$result['openid']);
                     die;
                 }
             }
