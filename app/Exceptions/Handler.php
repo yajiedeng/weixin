34222de82ab144b83bf43f531bdf36489ff57b25
dengyajie <dengyajie@incar100.com>
2018-05-28 16:02:24 +0800

    channel

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -48,9 +48,9 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
-        return parent::render($request, $exception);
-//        if ($exception instanceof ValidationException) {
-//            return new JsonResponse($exception->getMessage());
-//        }
+//        return parent::render($request, $exception);
+        if ($exception instanceof ValidationException) {
+            return new JsonResponse($exception->getMessage());
+        }
     }
 }
